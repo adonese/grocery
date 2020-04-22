4ee598b8b2fd4fb51cc58c45a42c9d70d36da46e
Mohamed Yousif <mmbusif@gmail.com>
2020-04-22 14:20:21 +0200

    adding cookies and user

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -14,7 +15,46 @@ func form(w http.ResponseWriter, r *http.Request) {
 	temp.Execute(w, data)
 }
 
+func register(w http.ResponseWriter, r *http.Request) {
+	var registerPage, err = template.ParseFiles("static/register.html")
+	if err != nil {
+		log.Fatalf("Template error: Error in parsing register page: %v", err)
+	}
+
+	errors := make(map[string]string)
+	errors["title"] = "Register - Grocery"
+	if r.Method == "GET" {
+		registerPage.Execute(w, errors)
+		return
+	}
+
+	// it is a post method
+	var u User
+	defer r.Body.Close()
+
+	// req, err := ioutil.ReadAll(r.Body)
+	// if err != nil {
+	// 	errors["register"] = err.Error()
+	// }
+
+	u.Username = r.PostFormValue("username")
+	u.Mobile = r.PostFormValue("mobile")
+	u.Telegram = r.PostFormValue("telegram")
+	if u.Username == "" || u.Mobile == "" {
+		errors["error"] = "empty username or mobile"
+		registerPage.Execute(w, nil)
+		return
+	}
+
+	// save the result to database
+	cookie := u.generateCookie()
+	log.Printf("The cookie is: %#v", cookie)
+	http.SetCookie(w, cookie)
+	registerPage.Execute(w, errors)
+}
+
 func main() {
 	http.HandleFunc("/", form)
+	http.HandleFunc("/register", register)
 	http.ListenAndServe(":8080", nil)
 }
--- types.go
+++ types.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"net/http"
+	"time"
+)
+
+// User struct in the system
+type User struct {
+	Username string
+	Mobile   string
+	Telegram string
+}
+
+func (u User) generateCookie() *http.Cookie {
+	c := &http.Cookie{
+		Name:       "grocery",
+		Value:      u.Username,
+		Path:       "/",
+		Domain:     "",
+		Expires:    time.Now().Add(1000 * time.Hour),
+		RawExpires: "",
+		MaxAge:     0,
+		// Secure:     true,
+		// HttpOnly:   true,
+		SameSite: 0,
+		Raw:      "",
+		Unparsed: nil,
+	}
+	return c
+}
